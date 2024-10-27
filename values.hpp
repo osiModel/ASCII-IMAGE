8e26347a1c7cc57f25dc6b18e733588a435c7020
osi_model <wetx227@gmail.com>
2024-10-27 18:41:31 +0200

    Update values.hpp

--- values.hpp
+++ values.hpp
@@ -8,14 +8,19 @@
 #include <array>
 
 namespace{
-    using Pixel = bool;
-    using std::vector;
-    using std::array;
     using std::pair; 
+    using std::array;
+    using std::vector;
+    using Pixel = bool;
     using Pos = pair<uint16_t,uint16_t>; 
+    using Map = vector<vector<Pixel>>;
     //constexpr std::string_view CHARS = "BW"; - black and white
     //constexpr float DIV = 256/CHARS.size(); - for more types of colors
 }
+
+
+
+bool NextStep(const Map& ,const Pos&);
 
 vector<Pos> Calculate(const vector<vector<Pixel>>& ,int32_t& ,const vector<uint16_t>&);
 

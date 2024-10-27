9534460dd52d35a9e8fbb9a2d1e5798ef372e9ec
osi_model <wetx227@gmail.com>
2024-10-27 17:45:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,26 +6,28 @@
 int main()
 {
     cv::Mat image; 
-    vector<Pos> enter; 
+    Pos start {-1, -1};
+    Pos end {-1, -1}; 
     int32_t simplestPath{};
-    image = cv::imread("../images/testmaze_1.png",cv::IMREAD_GRAYSCALE); 
-    vector<vector<Pixel>> map(image.cols,std::vector<Pixel>(image.rows));
+    image = cv::imread("../images/color_1.png",cv::IMREAD_GRAYSCALE); 
+    Map map(image.cols, vector<Pixel>(image.rows));
 
-    for(int i = 0;i<image.cols;++i){
-        for(int j = 0;j<image.rows;++j){
-            map[i][j] = (image.at<uint8_t>(i,j) <= 128);
+    for(int32_t i = 0;i<image.cols;++i){
+        for(int32_t j = 0;j<image.rows;++j){
+            uint8_t greyPixel = image.at<uint8_t>(i,j);
+            map[i][j] = (greyPixel >= 180);
+            std::cout<<map[i][j]<<" ";
             if((i == 0 || i == image.cols-1 || 
-                j == 0 || j == image.rows-1) && !map[i][j]){
-                    enter.push_back({i,j});           
+                j == 0 || j == image.rows-1) && map[i][j] >= 40 && map[i][j] <= 180){
+                    if(start.first == -1){
+                        start = {i,j};
+                    } else {
+                        end = {i,j};
+                    }
             }
         }
-    }   
-
-    if(enter.size() < 2){
-        std::cerr<<"LESS THEN TWO ENTERS";
-        return EXIT_FAILURE;
+        std::cout<<std::endl;
     }
-
 
     /*
     std::cout<<"PATH IS FINDED!\n";

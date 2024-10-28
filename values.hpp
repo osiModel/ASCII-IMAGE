02ef6273c6c130fa8a108ad2c45b42ee81e46e68
osi_model <wetx227@gmail.com>
2024-10-28 14:14:24 +0200

    Update values.hpp

--- values.hpp
+++ values.hpp
@@ -14,14 +14,24 @@ namespace{
     using Pixel = bool;
     using Pos = pair<uint16_t,uint16_t>; 
     using Map = vector<vector<Pixel>>;
+    using crint = const int32_t&;
+    using crpos = const Pos&;
+
+    struct Cell{
+        uint16_t yParent{}, xParent{};
+        double f{},g{},h{}; 
+    };
     //constexpr std::string_view CHARS = "BW"; - black and white
     //constexpr float DIV = 256/CHARS.size(); - for more types of colors
 }
 
 
+inline bool InRange(crpos, crpos);
+inline bool IsWall(const Map&, crpos);
+inline bool IsEnd(crpos, crpos);
+inline double HValue(crint, crint, crpos);
 
-bool NextStep(const Map& ,const Pos&);
 
-vector<Pos> Calculate(const vector<vector<Pixel>>& ,int32_t& ,const vector<uint16_t>&);
+vector<Pos> Calculate(const Map& ,size_t& ,const array<Pos, 2>&);
 
 #endif // VALUES_HPP

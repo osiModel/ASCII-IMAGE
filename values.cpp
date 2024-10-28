2ffb77e6e7c7e17b5f6d6f1f1158b637c852a0ed
osi_model <wetx227@gmail.com>
2024-10-28 14:14:14 +0200

    Update values.cpp

--- values.cpp
+++ values.cpp
@@ -1,7 +1,32 @@
 #include "values.hpp"
+#include <cmath>
 
-vector<Pos> Calculate(const vector<vector<Pixel>>& map,
-            int32_t& simplestPath,
-            const vector<uint16_t>& enter){
-    
+inline bool InRange(crpos pos, crpos range){
+    return ((pos.first < range.first) && (pos.second < range.second) && (pos.second >= 0) && (pos.first >= 0));
+}
+
+inline bool IsWall(const Map& map, const Pos& pos){
+    return map[pos.first][pos.second] == 1;
+}
+
+inline bool IsEnd(crpos start, crpos end){
+    return start == end;
+}
+
+double HValue(crpos pos, crpos end){
+    return  sqrt(pow((pos.first - end.first),2) + pow((pos.second - end.second),2));  
+}
+
+
+vector<Pos> Calculate(const Map& map, 
+size_t& simplestPath, 
+const array<Pos,2>& enter){
+    vector<Pos> result;
+
+
+
+
+
+    simplestPath = result.size();
+    return result;            
 }

cf6a7f71e4c372a960d685e904ae5ed98d85955b
osi_model <wetx227@gmail.com>
2024-11-05 18:50:35 +0200

    Update path.hpp

--- include/drawing/path.hpp
+++ include/drawing/path.hpp
@@ -1,6 +1,18 @@
 #ifndef PATH_HPP
 #define PATH_HPP
 
+#include "alike/identical.hpp"
+#include <opencv2/opencv.hpp>
+#include <string>
 
+namespace {
+    using cv::Mat;
+    using std::string;
+}
 
-#endif
+inline void SaveImg(const cv::Mat&, const std::string&);
+
+void DrawPath(const cv::Mat&, const vector<Pos>&);
+uint16_t CheckSize(const Pos&, const Map&);
+
+#endif // PATH_HPP

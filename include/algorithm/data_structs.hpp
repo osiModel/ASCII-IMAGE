11a1d4b5057499fa2a322338c8293a15df57d549
osi_model <wetx227@gmail.com>
2024-11-05 18:50:15 +0200

    Create data_structs.hpp

---
+++ include/algorithm/data_structs.hpp
@@ -0,0 +1,16 @@
+#ifndef DATA_STRUCTS_HPP
+#define DATA_STRUCTS_HPP
+
+#include "alike/identical.hpp"
+
+namespace{
+    struct Cell{
+        uint16_t yParent{}, xParent{};
+        double f{},g{},h{}; 
+    };
+
+    vector<vector<int8_t>> DIRECTIONS = {{-1, 0}, {1, 0}, {0, 1}, {0, -1},
+                                        {-1, 1}, {-1, -1}, {1, 1}, {1, -1}};
+}
+
+#endif // DATA_STRUCTS_HPP

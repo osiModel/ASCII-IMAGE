b8d66736e08087f4111587af9cd50a078e2cea75
osi_model <wetx227@gmail.com>
2024-11-05 18:49:51 +0200

    Create identical.hpp

---
+++ include/alike/identical.hpp
@@ -0,0 +1,16 @@
+#ifndef IDENTICAL_HPP
+#define IDENTICAL_HPP
+
+#include <cstdint>
+#include <vector>
+#include <utility>
+
+namespace{
+    using std::pair; 
+    using std::vector;
+    using Pos = pair<uint16_t,uint16_t>; 
+    using Pixel = bool;
+    using Map = vector<vector<Pixel>>;
+}
+
+#endif // IDENTICAL_HPP

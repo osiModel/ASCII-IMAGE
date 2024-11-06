fcdfb6644a59226165d6fba64d2187871f41d404
osiModel <wetx227@gmail.com>
2024-11-06 19:08:50 +0200

    Working, but without scaling

--- include/drawing/path.hpp
+++ include/drawing/path.hpp
@@ -6,13 +6,13 @@
 #include <string>
 
 namespace {
-    using cv::Mat;
     using std::string;
 }
 
-inline void SaveImg(const cv::Mat&, const std::string&);
+inline void SaveImg(const cv::Mat&, const string&);
 
 void DrawPath(const cv::Mat&, const vector<Pos>&);
-uint16_t CheckSize(const Pos&, const Map&);
+
+uint16_t CheckSize(const Pos&, const Map&); // todo
 
 #endif // PATH_HPP
--- src/path.cpp
+++ src/path.cpp
@@ -1,1 +1,22 @@
 #include "drawing/path.hpp"
+
+namespace {
+    const cv::Vec3b COLOR = {255, 0 , 0};
+    const string PATH = "../result/IMAGE.png";
+}
+
+inline void SaveImg(const cv::Mat& image, const string& path){
+    cv::imwrite(path, image);
+}
+
+void DrawPath(const cv::Mat& imageReference, const vector<Pos>& mazePath){   
+    
+    cv::Mat image = imageReference.clone();
+
+    for(const auto& cord : mazePath){
+        image.at<cv::Vec3b>(cord.first, cord.second) = COLOR;
+    }
+
+
+    SaveImg(image, PATH);
+}

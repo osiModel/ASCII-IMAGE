fd005cef798501e0b8a3f01c9ec47ef9ce101386
osiModel <wetx227@gmail.com>
2024-11-06 19:14:12 +0200

    Adding drawing func

--- main.cpp
+++ main.cpp
@@ -1,28 +1,32 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
-#include "algorithm/values.hpp"
+#include "algorithm/astar.hpp"
 #include "drawing/path.hpp"
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <string> 
 
-int main(int argc, char** argv){
-    if(argc != 2){
-        std::cerr<<"Enter the img path!\n";
-        return EXIT_FAILURE;
+int main(int argc, char** argv){ 
+    std::string imagePath = "../images/testmaze_2.png";
+    bool defaultPath = true;
+
+    if(argc == 2){
+        imagePath = argv[1];
+        defaultPath = false;
     }
 
-    cv::Mat image; 
+    cv::Mat image = cv::imread(imagePath);
+    cv::Mat greyImage = cv::imread(imagePath,cv::IMREAD_GRAYSCALE); 
     std::vector<Pos> path;
     bool status = false;
     Pos start {INT16_MAX, INT16_MAX};
     Pos end {INT16_MAX, INT16_MAX}; 
-    image = cv::imread(argv[1],cv::IMREAD_GRAYSCALE); 
-    Map map(image.cols, std::vector<Pixel>(image.rows));
+    Map map(greyImage.cols, std::vector<Pixel>(greyImage.rows));
     
-    for(size_t i = 0;i<image.cols;++i){
-        for(size_t j = 0;j<image.rows;++j){
-            uint8_t greyPixel = image.at<uint8_t>(i,j);
+    for(size_t i = 0;i<greyImage.cols;++i){
+        for(size_t j = 0;j<greyImage.rows;++j){
+            uint8_t greyPixel = greyImage.at<uint8_t>(i,j);
             map[i][j] = (greyPixel >= 180);
             if(greyPixel >= 40 && greyPixel <= 180){
                     if(start.first == INT16_MAX){
@@ -35,16 +39,17 @@ int main(int argc, char** argv){
                     map[i][j] = 1;
             }
 
-            std::cout<<map[i][j]<<" ";
+            //std::cout<<map[i][j]<<" ";
         }
         
-        std::cout<<std::endl;
+        //std::cout<<std::endl;
     }
 
-    if(status) 
+    if(status){
         path = Calculate(map,start,end);
 
-
+        DrawPath(image,path);
+    } 
 
     return EXIT_SUCCESS;
 }

fabc69b2c09eeac31a1416371855768bcb16a1e6
osi_model <wetx227@gmail.com>
2024-11-11 18:32:42 +0200

    Update path.cpp

--- src/path.cpp
+++ src/path.cpp
@@ -1,22 +1,50 @@
 #include "drawing/path.hpp"
 
-namespace {
-    const cv::Vec3b COLOR = {255, 0 , 0};
-    const string PATH = "../result/IMAGE.png";
+inline bool IsIncreasing(const Pos& range, const Pos& dot){
+    uint32_t y = dot.first;
+    uint32_t x = dot.second;
+    
+    return !(y >= range.first || x >= range.second);
+}
+
+inline bool IsValid(const GreyMap& map,const Pos& dot){
+    uint32_t y = dot.first;
+    uint32_t x = dot.second;
+
+    return(map[y][x] >= 40 || map[y][x] <= 180);
+}
+
+uint16_t CheckSize(const GreyMap& map, const Pos& dot){
+    uint16_t size = 1;
+    uint16_t y = dot.first;
+    uint16_t x = dot.second;
+    Pos range = {map.size(), map[0].size()};
+
+
+    while(IsValid(map, {y+size,x+size}) && IsIncreasing(range, {y+size,x+size})){
+        ++size;
+    }
+
+    return size;
 }
 
 inline void SaveImg(const cv::Mat& image, const string& path){
     cv::imwrite(path, image);
 }
 
-void DrawPath(const cv::Mat& imageReference, const vector<Pos>& mazePath){   
-    
+void DrawPath(const cv::Mat& imageReference, const vector<Pos>& mazePath, const uint16_t& size){   
     cv::Mat image = imageReference.clone();
 
+    uint16_t counter{};
     for(const auto& cord : mazePath){
-        image.at<cv::Vec3b>(cord.first, cord.second) = COLOR;
+        if(counter%size == 0){
+            for(size_t i = 0;i<size;++i){
+                for(size_t j = 0;j<size;++j){
+                    image.at<cv::Vec3b>(cord.first+i, cord.second+j) = COLOR;
+                }
+            }
+        }
+        ++counter;
     }
-
-
     SaveImg(image, PATH);
 }

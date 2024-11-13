a65d06345f441b541e421e888ccfaabd8f379b3d
osi_model <wetx227@gmail.com>
2024-11-13 18:30:38 +0200

    Update astar.hpp

--- include/algorithm/astar.hpp
+++ include/algorithm/astar.hpp
@@ -1,29 +1,41 @@
-#ifndef VALUES_HPP
-#define VALUES_HPP 
+#ifndef ASTAR_HPP
+#define ASTAR_HPP 
 
 #include "alike/identical.hpp"
 #include "data_structs.hpp"
+#include <array>
+#include <set>
 
 namespace{
     using Details = vector<vector<Cell>>;
+    using dPair = pair<double,pair<uint16_t,uint16_t>>; 
+    using std::array;
+    using std::set;
 }
 
-/*
-THESE FUNCS ARE NOT IMPLEMENTATION, SO I DON'T NEED THEM HERE
+class AStar{
+public:
+    AStar() = default;
 
-inline bool InRange(const Pos&, const Pos&);
-inline bool IsWall(const Map&, const Pos&);
-inline bool IsEnd(const Pos&, const Pos&);
-inline double HValue(const Pos&, const Pos&);
+    void PrintPath(const Details&, const Pos&);
+    vector<Pos> Calculate(const Map&, const Pos&, const Pos&, const bool& diagonal = false);
 
-void DirectionBlueprint(const array<Pos, 4>&, Details&,
-                        bool&, set<dPair>&, const Map&, const Map&, 
-                        double&, double&, double&);
-*/
+private:
+    inline bool InRange(const Pos&, const Pos&);
+    inline bool IsWall(const Map&, const Pos&);
+    inline bool IsEnd(const Pos&, const Pos&);
+    inline double HValue(const Pos&, const Pos&);
 
-void PrintPath(const Details&, const Pos&);
+    vector<Pos> Path(const Details&, const Pos&, const Pos&);
+    void DirectionBlueprint(const array<Pos, 4>&, 
+                        Details&, 
+                        bool& founded, 
+                        set<dPair>&, 
+                        const Map&,
+                        const Map&,
+                        double&,
+                        double&,
+                        double&);
+};  
 
-vector<Pos> Calculate(const Map&, const Pos&, const Pos&, const bool& diagonal = false);
-vector<Pos> Path(const Details&, const Pos&, const Pos&);
-
-#endif // VALUES_HPP
+#endif // ASTAR_HPP

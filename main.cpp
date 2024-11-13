1f28c8e6d22f1d55df76affe0d2dc32e5c6c6085
osi_model <wetx227@gmail.com>
2024-11-13 18:34:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,19 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include "algorithm/astar.hpp"
-#include "drawing/path.hpp"
-#include <iostream>
-#include <vector>
-#include <cstdint>
-#include <string> 
+#include "path_finder.hpp"
+
+namespace {
+    const std::string IMAGE_PATH = "../images/testmaze_5.png";
+}
+
 
 int main(int argc, char** argv){ 
-    std::string imagePath = "../images/testmaze_5.png";
-    bool defaultPath = true;
+    std::string imagePath = IMAGE_PATH;
 
     if(argc == 2){
         imagePath = argv[1];
-        defaultPath = false;
     }
 
-    cv::Mat image = cv::imread(imagePath);
-    cv::Mat greyImage = cv::imread(imagePath,cv::IMREAD_GRAYSCALE); 
-    std::vector<Pos> path;
-    bool status = false;
-    Pos start {INT16_MAX, INT16_MAX};
-    Pos end {INT16_MAX, INT16_MAX}; 
-    Map map(greyImage.cols, std::vector<Pixel>(greyImage.rows));
-    GreyMap greyMap(greyImage.cols, std::vector<uint8_t>(greyImage.rows));
-
-    for(size_t i = 0;i<greyImage.cols;++i){
-        for(size_t j = 0;j<greyImage.rows;++j){
-            uint8_t greyPixel = greyImage.at<uint8_t>(i,j);
-            map[i][j] = (greyPixel >= 180);
-            greyMap[i][j] = greyPixel;
-            if(greyPixel >= 40 && greyPixel <= 180){
-                    if(start.first == INT16_MAX){
-                        start = {i,j};
-                    } else {
-                        end = {i,j};
-                        status = true;
-                    }
-                    map[i][j] = 1;
-            }
-        }
-    }
-    if(status){
-        path = Calculate(map,start,end);
-        
-        DrawPath(image,path);
-    } 
-
-    return EXIT_SUCCESS;
+    PathFinder pf(IMAGE_PATH);
+    pf.DrawPath();
+    pf.FindPath();
+    pf.DrawPath();
 }

e38f8256ce781c55e0e6c8bf066965dbfb29feae
osi_model <wetx227@gmail.com>
2024-11-16 18:44:32 +0200

    final cut

--- main.cpp
+++ main.cpp
@@ -1,19 +1,15 @@
-#include "path_finder.hpp"
+#include "pf/path_finder.hpp"
 
-namespace {
-    const std::string IMAGE_PATH = "../images/testmaze_5.png";
-}
-
-
-int main(int argc, char** argv){ 
-    std::string imagePath = IMAGE_PATH;
+int main(int argc,char** argv){
+    string path = "/home/admin/Desktop/c++/projects/Path_Finder/images/testlargemaze_2.png"; 
 
     if(argc == 2){
-        imagePath = argv[1];
+        path = argv[2];
     }
 
-    PathFinder pf(IMAGE_PATH);
-    pf.DrawPath();
+    PathFinder pf;
+    pf.SetImagePath(path);
+    pf.SetSavePath("../result/IMAGE.png");
     pf.FindPath();
     pf.DrawPath();
 }

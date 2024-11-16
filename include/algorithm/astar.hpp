9cc3a430ca35c41d21981d376a81d837867e7ac7
osi_model <wetx227@gmail.com>
2024-11-16 18:40:01 +0200

    refactoring

--- include/algorithm/astar.hpp
+++ include/algorithm/astar.hpp
@@ -5,26 +5,34 @@
 #include "data_structs.hpp"
 #include <array>
 #include <set>
+#include <cmath>
 
 namespace{
     using Details = vector<vector<Cell>>;
     using dPair = pair<double,pair<uint16_t,uint16_t>>; 
     using std::array;
     using std::set;
+    using std::pow;
+    using std::sqrt;
 }
 
-class AStar{
+class AStar{    
 public:
     AStar() = default;
-
-    void PrintPath(const Details&, const Pos&);
     vector<Pos> Calculate(const Map&, const Pos&, const Pos&, const bool& diagonal = false);
-
 private:
-    inline bool InRange(const Pos&, const Pos&);
-    inline bool IsWall(const Map&, const Pos&);
-    inline bool IsEnd(const Pos&, const Pos&);
-    inline double HValue(const Pos&, const Pos&);
+    bool InRange(const Pos& pos, const Pos& range){
+        return ((pos.first <= range.first) && (pos.second <= range.second) && (pos.second >= 0) && (pos.first >= 0));
+    }
+    bool IsWall(const Map& map, const Pos& pos){
+        return (map[pos.first][pos.second] == 0);
+    }
+    bool IsEnd(const Pos& start, const Pos& end){
+        return (start == end);
+    }
+    double HValue(const Pos& pos, const Pos& end){
+        return  (sqrt(pow((end.first - pos.first),2) + pow((end.second - pos.second),2)));  
+    }
 
     vector<Pos> Path(const Details&, const Pos&, const Pos&);
     void DirectionBlueprint(const array<Pos, 4>&, 
@@ -36,6 +44,7 @@ private:
                         double&,
                         double&,
                         double&);
+
 };  
 
 #endif // ASTAR_HPP

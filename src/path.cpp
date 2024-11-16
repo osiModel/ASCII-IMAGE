e0f6f7ce67d6dad643eb24ab889c7bcb96e8f574
osi_model <wetx227@gmail.com>
2024-11-16 18:43:31 +0200

    final cut

--- src/path.cpp
+++ src/path.cpp
@@ -1,15 +1,23 @@
 #include "drawing/path.hpp"
 
-inline void Painter::SaveImg(const cv::Mat& image, const string& path){
-    cv::imwrite(path, image);
-}
+Painter::Painter(const string& savePath) : m_savePath(savePath){}
 
 void Painter::DrawPath(const cv::Mat& imageReference, const vector<Pos>& mazePath){   
     cv::Mat image = imageReference.clone();
-
-    for(const auto& cord : mazePath){
-                    image.at<cv::Vec3b>(cord.first, cord.second) = COLOR;
+    if(m_pixelSize == 1){
+        for(const auto& cord : mazePath){
+            image.at<cv::Vec3b>(cord.first, cord.second) = COLOR;
+        }
+    }else{
+        for(const auto& cord : mazePath){
+            uint16_t x = m_pixelSize * cord.second;
+            uint16_t y = m_pixelSize * cord.first;
+            for(uint16_t i = y;i<y+m_pixelSize;++i){
+                for(uint16_t j = x;j<x+m_pixelSize;++j){
+                    image.at<cv::Vec3b>(i, j) = COLOR;
+                }
+            }
+        }
     }
-    
-    SaveImg(image, PATH);
+    SaveImg(image, SAVE_PATH);
 }
